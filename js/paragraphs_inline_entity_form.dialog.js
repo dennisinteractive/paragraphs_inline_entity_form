92677e330978df7d653fb576f5b80613eb49e6c8
Marcelo Vani <marcellovani@yahoo.co.uk>
2020-11-25 16:37:53 +0000

    Issue #3096667 by marcelovani: Edited Paragraphs Not Updating In WYSIWYG Editor

--- js/paragraphs_inline_entity_form.dialog.js
+++ js/paragraphs_inline_entity_form.dialog.js
@@ -10,4 +10,14 @@
     this.form.elements.namedItem('selected_bundle').value = this.attributes['data-paragraph-bundle'].value;
   });
 
+  // When we are editing an entity that was already on the body copy, the Embed button doesn't update the changes.
+  // This happens because when we open the second Modal Dialog, the first one loses the reference.
+  // This is a little trick to reinstate the callback. Its not beautiful, but works beautifully.
+  $(window).on('editor:dialogsave', function (e, values) {
+    if (typeof window.ckeditorSaveCallback == 'function') {
+      Drupal.ckeditor.saveCallback = window.ckeditorSaveCallback;
+      Drupal.ckeditor.saveCallback(values);
+    }
+  });
+
 })(jQuery);
